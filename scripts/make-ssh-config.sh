aea18c89453360a39b330dda86857086e72ddb1a
Toby Ferguson <toby@cloudera.com>
2018-12-06 07:07:22 -0800

    Initial commit to generate ssh config file better

--- scripts/make-ssh-config.sh
+++ scripts/make-ssh-config.sh
@@ -1,8 +1,10 @@
+. $(dirname $0)/../cluster.properties
 
-PREFIX=${1:?"No name prefix supplied"}
-gcloud compute instances list --filter="name~^${PREFIX}.*" --format="[table](name:sort=1,networkInterfaces[0].accessConfigs[0].natIP)" |
-    grep "${PREFIX:?}" |
-    sed 's/\(.*\)  \(.*\)/Host \1 \
+# Use jq to number the different roles
+
+#PREFIX=${1:?"No name prefix supplied"}
+gcloud compute instances list --filter="name~^${PREFIX}.*" --format="[table,no-heading](metadata.items[2].value:sort=1,networkInterfaces[0].accessConfigs[0].natIP)" |
+    sed 's/\(.*\)  \(.*\)/Host '${PREFIX:?}'-\1 \
 	Hostname \2/g'
 
 cat <<EOF
